a6ac2b596327e3abab9ae1e037e5b21219f08965
Eric Hugoson <eric@hugoson.org>
2021-03-05 15:39:43 +0100

    added test page

--- app/routes.py
+++ app/routes.py
@@ -1,12 +1,18 @@
-from tornado.wsgi import WSGIContainer
-from tornado.httpserver import HTTPServer
-from tornado.ioloop import IOLoop
+#from tornado.wsgi import WSGIContainer
+#from tornado.httpserver import HTTPServer
+#from tornado.ioloop import IOLoop
 from app import app
+from flask import render_template
 
 @app.route('/')
 @app.route('/index')
 def index():
     return "There seems to be nothing here."
+
+@app.route('/test')
+def test():
+    return render_template('index.html')
+
 #http_server = HTTPServer(WSGIContainer(app))
 #http_server.listen(int(5000),address="198.54.116.15")
 #IOLoop.instance().start()
