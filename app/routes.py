68f53227596ebc16bd2756b6904e8fcd5e39164e
Eric Hugoson <eric@hugoson.org>
2021-03-10 19:17:23 +0100

    made new page default

--- app/routes.py
+++ app/routes.py
@@ -6,16 +6,8 @@ from flask import render_template
 
 @app.route('/')
 @app.route('/index')
-def index():
-    return "There seems to be nothing here."
-
-@app.route('/test')
 def test():
     return render_template('index.html')
-
-#http_server = HTTPServer(WSGIContainer(app))
-#http_server.listen(int(5000),address="198.54.116.15")
-#IOLoop.instance().start()
 
 if __name__ == "__main__":
 	app.run()
